31899a185950d494081cb7eb669f335ccb060e8b
Yicheng Jin <bobjin42@gmail.com>
2018-05-09 21:26:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string){
     return "I can'\t hear you!"
+  } else if {
+    string.toLowerCase() === string){
+      return 
+    }
   }
 }
 
-function sayHiToGrandma(string) {
-  if(string.toUpperCase() === string){
-    return "YES INDEED!"
-  }
-}
